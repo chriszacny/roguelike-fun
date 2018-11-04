1f1d920840bee9322234f2c60bceab5a27bf9eb2
Chris Zacny <czacny@gmail.com>
2018-11-04 10:13:55 -0800

    Saving some images

--- game.py
+++ game.py
@@ -7,10 +7,12 @@ def main():
     #logo = pygame.image.load("logo32x32.png")
     #pygame.display.set_icon(logo)
     pygame.display.set_caption("Zelda Dungeon")
-
-    image = pygame.image.load(os.path.join('images', 'link-down64x64.png'))
-    screen = pygame.display.set_mode((800, 600))
-    screen.blit(image, (50, 50))
+    
+    image_hero = pygame.image.load(os.path.join('images', 'link-down64x64.png'))
+    image_spider = pygame.image.load(os.path.join('images', 'spider-down-64x64.png'))
+    screen = pygame.display.set_mode((640, 640))
+    screen.blit(image_hero, (256, 256))
+    screen.blit(image_spider, (64, 64))
     pygame.display.flip()
 
     running = True
